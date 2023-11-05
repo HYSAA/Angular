404bb3ab217637a5039784ca8a88f23d8229d154
Alberto Anunciado Jr <aj.anunciado@gmail.com>
2023-11-05 15:45:10 +0800

    adasdasd

--- calculator/src/app/app.component.ts
+++ calculator/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'calculator';
+export class AppComponent implements OnInit {
+
+  firstOperand: number = 0;
+  secondOperand: number = 0;
+  operator: string = '+';
+  result: number = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  calculate() {
+    switch (this.operator) {
+      case '+':
+        this.result = this.firstOperand + this.secondOperand;
+        break;
+      case '-':
+        this.result = this.firstOperand - this.secondOperand;
+        break;
+      case '*':
+        this.result = this.firstOperand * this.secondOperand;
+        break;
+      case '/':
+        this.result = this.firstOperand / this.secondOperand;
+        break;
+    }
+  }
 }
